63d2872d5330a129107da4240205520ab5a8d136
4bselyam <4bselyam@gmail.com>
2020-12-05 02:30:01 +0200

    first commit

---
+++ ahmedblog/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8b566ebd3b042192ae9a4c774d36ea21'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from ahmedblog import routes
