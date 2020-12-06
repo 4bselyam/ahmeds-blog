b56100c3f62095d97b07eeb29dd5e332ee65b8e1
4bselyam <4bselyam@gmail.com>
2020-12-06 15:08:31 +0200

    email reset password

--- ahmedblog/__init__.py
+++ ahmedblog/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8b566ebd3b042192ae9a4c774d36ea21'
@@ -11,5 +13,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = '4bselyam@gmail.com'
+app.config['MAIL_PASSWORD'] = 'aabselyam2001'
+mail = Mail(app)
 
 from ahmedblog import routes
